e862574342b24ee29c261cedd35a00c0626f01f5
ArmNo <monkeyarmno@gmail.com>
2014-02-05 19:09:56 +0700

    concat and uglify js into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var prefix = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 
 gulp.task('css', function() {
 	return gulp.src('sass/style.sass')
@@ -9,9 +11,21 @@ gulp.task('css', function() {
 				.pipe(gulp.dest('css'));
 });
 
+gulp.task('js', function() {
+	return gulp.src(['js/lib/fastclick/lib/fastclick.js'])
+				.pipe(concat('scripts.js'))
+				.pipe(uglify())
+				.pipe(gulp.dest('js'));
+});
+
 gulp.task('default', function() {
 	gulp.run('css');
+	gulp.run('js');
 	gulp.watch('sass/**/*', function() {
 		gulp.run('css');
+	});
+
+	gulp.watch('js/**/*', function() {
+		gulp.run('js');
 	});
 });
