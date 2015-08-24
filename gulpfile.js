92327c9f1fc49ac8ea5c483d5f4375c07099d3a1
Armno <monkeyarmno@gmail.com>
2015-08-24 13:58:15 +0700

    added flexbox mixins. meh

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,10 @@
 var gulp = require('gulp');
 
-// load plugins
-var $ = require('gulp-load-plugins')();
+gulp.task('jekyll', function(gulpCallback) {
+  var spawn = require('child_process').spawn;
+  var jekyll = spawn('jekyll', ['build'], { stdio: 'inherit'});
 
-gulp.task('css', function() {
-	return gulp.src('sass/style.sass')
-				.pipe($.rubySass({
-          style: 'compressed'
-        }))
-				.pipe(gulp.dest('css'));
-});
-
-gulp.task('js', function() {
-	return gulp.src(['js/lib/fastclick/lib/fastclick.js'])
-				.pipe($.concat('scripts.js'))
-				.pipe($.uglify())
-				.pipe(gulp.dest('js'));
-});
-
-gulp.task('default', ['css', 'js'], function() {
-	gulp.watch('sass/**/*', ['css']);
-	gulp.watch('js/**/*', ['js']);
-});
+  jekyll.on('exit', function(code) {
+    gulpCallback(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+ code)
+    })
+  });
