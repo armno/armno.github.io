2e4f2d361e5a8e7b7796f960b56c24c7edb09bb6
Armno <monkeyarmno@gmail.com>
2016-11-09 17:37:24 +0700

    add Show comments button to load disqus on demand

---
+++ js/app.js
@@ -0,0 +1,18 @@
+(function() {
+  function showDisqusComments() {
+    this.classList.add('hidden');
+    initDisqus();
+  }
+
+  function initDisqus() {
+    var disqus_shortname = 'armnointh';
+    (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+  }
+
+  document.querySelector('#js-show-comments')
+    .addEventListener('click', showDisqusComments);
+})();
